e2115491d70511eee8fa4ad6aef1e043b4a20ff9
Yee-Ting Li <yee379@gmail.com>
2019-01-06 22:40:29 -0800

    add post-hook custom script to run before jupyterlab is instantiated

---
+++ scripts/post-hook.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo 'Example script that after container setup, but before the jupyterlab instance is started.'
+echo "One may use \$\{HOME\} to to determine where the user's home directory is (${HOME})"
